35840a126443872bf10b420c43b51f216552123e
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-05-23 09:37:19 +0630

    Game interface

--- src/main.m
+++ src/main.m
@@ -40,6 +40,34 @@
 - (void)startButtonClicked:(id)sender {
     // Handle start button click
     [self.statusLabel setStringValue:@"Game Started!"];
+    
+    // Call the method to set up the game interface after a short delay to show the status label update
+    [self performSelector:@selector(setupGameInterface) withObject:nil afterDelay:1.0];
+}
+
+- (void)setupGameInterface {
+    // Remove or hide the start button and status label
+    [self.startButton setHidden:YES];
+    [self.statusLabel setHidden:YES];
+    
+    // Add game elements here
+    NSTextField *gameLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(50, 50, 300, 30)];
+    [gameLabel setEditable:NO];
+    [gameLabel setBezeled:NO];
+    [gameLabel setBackgroundColor:[NSColor clearColor]];
+    [gameLabel setAlignment:NSTextAlignmentCenter];
+    [gameLabel setStringValue:@"Game is now running..."];
+    [self.window.contentView addSubview:gameLabel];
+    
+    // You can add more game UI elements here
+    // Example: A simple label to represent a game element
+    NSTextField *gameElement = [[NSTextField alloc] initWithFrame:NSMakeRect(100, 100, 200, 30)];
+    [gameElement setEditable:NO];
+    [gameElement setBezeled:NO];
+    [gameElement setBackgroundColor:[NSColor clearColor]];
+    [gameElement setAlignment:NSTextAlignmentCenter];
+    [gameElement setStringValue:@"[Game Element]"];
+    [self.window.contentView addSubview:gameElement];
 }
 
 @end
