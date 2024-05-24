9b3fa9442259761a5d0472e78314fd33cb1bcbcd
ThomasNoSimp <thurawaizin.conceptx@gmail.com>
2024-05-24 13:08:14 +0630

    !

--- src/main.m
+++ src/main.m
@@ -1,8 +1,10 @@
 #import <Cocoa/Cocoa.h>
+#import <SpriteKit/SpriteKit.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (nonatomic, strong) NSWindow *window;
+@property (nonatomic, strong) SKView *skView;
 @property (nonatomic, strong) NSButton *startButton;
 @property (nonatomic, strong) NSTextField *statusLabel;
 
@@ -18,6 +20,10 @@
                                                   defer:NO];
     [self.window setTitle:@"SimulatorGame"];
     [self.window makeKeyAndOrderFront:nil];
+    
+    // Create an SKView to display SpriteKit content
+    self.skView = [[SKView alloc] initWithFrame:NSMakeRect(0, 0, 400, 200)];
+    [self.window.contentView addSubview:self.skView];
     
     // Create a start button
     self.startButton = [[NSButton alloc] initWithFrame:NSMakeRect(150, 100, 100, 30)];
@@ -50,22 +56,75 @@
     [self.startButton setHidden:YES];
     [self.statusLabel setHidden:YES];
     
-    // Add game elements here
-    NSTextField *gameLabel = [[NSTextField alloc] initWithFrame:NSMakeRect(50, 50, 300, 30)];
-    [gameLabel setEditable:NO];
-    [gameLabel setBezeled:NO];
-    [gameLabel setBackgroundColor:[NSColor clearColor]];
-    [gameLabel setAlignment:NSTextAlignmentCenter];
-    [gameLabel setStringValue:@"Game is now running..."];
-    [self.window.contentView addSubview:gameLabel];
+    // Create an SKScene instance
+    SKScene *gameScene = [[SKScene alloc] initWithSize:self.skView.bounds.size];
+    gameScene.scaleMode = SKSceneScaleModeAspectFill;
     
-    NSTextField *gameElement = [[NSTextField alloc] initWithFrame:NSMakeRect(100, 100, 200, 30)];
-    [gameElement setEditable:NO];
-    [gameElement setBezeled:NO];
-    [gameElement setBackgroundColor:[NSColor clearColor]];
-    [gameElement setAlignment:NSTextAlignmentCenter];
-    [gameElement setStringValue:@"[Game Element]"];
-    [self.window.contentView addSubview:gameElement];
+    // Add game elements to the scene
+    SKLabelNode *gameLabel = [SKLabelNode labelNodeWithText:@"Game is now running..."];
+    gameLabel.position = CGPointMake(CGRectGetMidX(gameScene.frame), CGRectGetMidY(gameScene.frame) + 100);
+    [gameScene addChild:gameLabel];
+    
+    // Draw a character using SKShapeNode
+    // Head
+    SKShapeNode *head = [SKShapeNode shapeNodeWithCircleOfRadius:20];
+    head.fillColor = [NSColor colorWithRed:0.94 green:0.85 blue:0.73 alpha:1.0]; // Skin tone color
+    head.position = CGPointMake(CGRectGetMidX(gameScene.frame), CGRectGetMidY(gameScene.frame) + 50);
+
+    // Eyes
+    SKShapeNode *leftEye = [SKShapeNode shapeNodeWithCircleOfRadius:3];
+    leftEye.fillColor = [NSColor blackColor];
+    leftEye.position = CGPointMake(-7, 5);
+
+    SKShapeNode *rightEye = [SKShapeNode shapeNodeWithCircleOfRadius:3];
+    rightEye.fillColor = [NSColor blackColor];
+    rightEye.position = CGPointMake(7, 5);
+
+    [head addChild:leftEye];
+    [head addChild:rightEye];
+
+    // Mouth
+    SKShapeNode *mouth = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(10, 2)];
+    mouth.fillColor = [NSColor blackColor];
+    mouth.position = CGPointMake(0, -10);
+
+    [head addChild:mouth];
+
+    // Body
+    SKShapeNode *body = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(20, 40)];
+    body.fillColor = [NSColor colorWithRed:0.94 green:0.85 blue:0.73 alpha:1.0]; // Skin tone color
+    body.position = CGPointMake(CGRectGetMidX(gameScene.frame), CGRectGetMidY(gameScene.frame) + 10); // Adjusted position
+
+    // Left arm
+    SKShapeNode *leftArm = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(10, 30)];
+    leftArm.fillColor = [NSColor colorWithRed:0.94 green:0.85 blue:0.73 alpha:1.0]; // Skin tone color
+    leftArm.position = CGPointMake(CGRectGetMidX(gameScene.frame) - 20, CGRectGetMidY(gameScene.frame) + 10); // Adjusted position
+
+    // Right arm
+    SKShapeNode *rightArm = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(10, 30)];
+    rightArm.fillColor = [NSColor colorWithRed:0.94 green:0.85 blue:0.73 alpha:1.0]; // Skin tone color
+    rightArm.position = CGPointMake(CGRectGetMidX(gameScene.frame) + 20, CGRectGetMidY(gameScene.frame) + 10); // Adjusted position
+
+    // Left leg
+    SKShapeNode *leftLeg = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(10, 30)];
+    leftLeg.fillColor = [NSColor colorWithRed:0.47 green:0.36 blue:0.29 alpha:1.0]; // Pants color
+    leftLeg.position = CGPointMake(CGRectGetMidX(gameScene.frame) - 5, CGRectGetMidY(gameScene.frame) - 30); // Adjusted position
+
+    // Right leg
+    SKShapeNode *rightLeg = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(10, 30)];
+    rightLeg.fillColor = [NSColor colorWithRed:0.47 green:0.36 blue:0.29 alpha:1.0]; // Pants color
+    rightLeg.position = CGPointMake(CGRectGetMidX(gameScene.frame) + 5, CGRectGetMidY(gameScene.frame) - 30); // Adjusted position
+    
+    // Add the body parts to the scene
+    [gameScene addChild:head];
+    [gameScene addChild:body];
+    [gameScene addChild:leftArm];
+    [gameScene addChild:rightArm];
+    [gameScene addChild:leftLeg];
+    [gameScene addChild:rightLeg];
+    
+    // Present the scene
+    [self.skView presentScene:gameScene];
 }
 
 @end
@@ -79,5 +138,3 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
-
-//
